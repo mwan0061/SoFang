654f30febd02575bfac855db84d6ed9f11e7f7c7
mwan0061 <mwan0061@gmail.com>
2016-04-15 19:52:03 +1000

    Added files via upload

---
+++ sf.js
@@ -0,0 +1,8 @@
+var sf = (function () {
+  'use strict';
+  var initModule = function ( $container ) {
+    sf.model.initModule();
+  };
+  
+  return { initModule : initModule };
+}());
---
+++ sf.shell.js
@@ -0,0 +1,16 @@
+sf.shell = (function () {
+  'use strict';
+  var
+    initModule,
+	swipeDown, swipeUP
+  ;
+  
+  initModule = function ( $container ) {
+  };
+  
+  return { 
+    initModule : initModule,
+	swipeDown  : swipeDown,
+	swipeUp    : swipeUp
+  };
+}());
