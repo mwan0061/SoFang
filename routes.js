ce675cb666fcacc246915b194bef0a6c6baeeb5d
Max Wan <mwan0061@gmail.com>
2016-04-23 21:14:49 +1000

    adds JSV

--- routes.js
+++ routes.js
@@ -1,8 +1,11 @@
 var
   configRoutes,
-  mongoClient = require( 'mongodb' ).MongoClient,
+  mongodb     = require( 'mongodb' ),
+  mongoClient = mongodb.MongoClient,
   mongoDbUrl  = 'mongodb://localhost:27017/sf',
-  dbHandle
+  dbHandle,
+  makeMongoId = mongodb.ObjectID,
+  objTypeMap  = { 'user': {} }
 ;
 
 mongoClient.connect(mongoDbUrl, function( err, db ) {
@@ -22,11 +25,103 @@ configRoutes = function ( app, server ) {
 
   app.all( '/api/*?', function ( request, response, next ) {
     response.contentType( 'json' );
-	next();
+    if ( objTypeMap[ request.params.obj_type ] ) {
+      next();
+    }
+	  else {
+      response.send({ error_msg : request.params.obj_type
+        + ' is not a valid object type' });
+    }
   });
 
-  app.get( '/api/list/:obj_type/:id([0-9]+)', function ( request, response ) {
-    response.send({ title : request.params.obj_type + ' with id ' + request.params.id });
+  app.get( '/api/:obj_type/list', function ( request, response ) {
+    dbHandle.collection(
+      request.params.obj_type,
+      function ( outer_error, collection ) {
+        collection.find().toArray(
+          function ( inner_error, map_list ) {
+            response.send( map_list );
+          }
+        );
+      }
+    );
+  });
+
+  app.get( '/api/:obj_type/read/:id', function ( request, response) {
+    var find_map = { _id: makeMongoId( request.params.id ) };
+    dbHandle.collection(
+      request.params.obj_type,
+      function ( outer_error, collection ) {
+        collection.findOne(
+          find_map,
+          function ( inner_error, result_map ) {
+            response.send( result_map );
+          }
+        );
+      }
+    );
+  });
+
+  app.post( '/api/:obj_type/create', function ( request, response ) {
+    dbHandle.collection(
+      request.params.obj_type,
+      function ( outer_error, collection ) {
+        var
+          options_map = { w : 1 },
+          obj_map     = request.body
+        ;
+
+        collection.insert(
+          obj_map,
+          options_map,
+          function ( inner_error, result_map ) {
+            response.send(result_map);
+          }
+        );
+      }
+    );
+  });
+
+  app.post( '/api/:obj_type/update/:id', function ( request, response ) {
+    dbHandle.collection(
+      request.params.obj_type,
+      function ( outer_error, collection ) {
+        var
+          find_map    = { _id: makeMongoId( request.params.id ) },
+          sort_order  = [],
+          options_map = { w: 1, upsert: false },
+          obj_map     = request.body
+        ;
+        collection.findAndModify(
+          find_map,
+          sort_order,
+          obj_map,
+          options_map,
+          function ( inner_error, updated_map ) {
+            response.send( updated_map );
+          }
+        );
+      }
+    );
+  });
+
+  app.get( '/api/:obj_type/delete/:id', function ( request, response) {
+    var find_map = { _id: makeMongoId( request.params.id ) };
+
+    dbHandle.collection(
+      request.params.obj_type,
+      function ( outer_error, collection ) {
+        var options_map = { w: 1, single: true };
+
+        collection.remove(
+          find_map,
+          options_map,
+          function ( inner_error, delete_count ) {
+            response.send( { delete_count: delete_count } );
+          }
+        );
+      }
+    );
   });
 };
 
